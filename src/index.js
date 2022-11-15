f7c1cbd8030a9dbcdea498380d7957bf997a6ac0
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-11-15 23:06:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       date: new Date(),
+//     };
+//   }
+//   render() {
+//     return <p>{this.state.date.toLocaleDateString()}</p>;
+//   }
+// }
+// ReactDOM.render(<Clock />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
