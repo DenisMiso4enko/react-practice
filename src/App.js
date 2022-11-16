06e262232c21b1bca0bdac5478c449d07af6bd99
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-11-16 22:17:37 +0300

    feat: add solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.intervalFlag = null;
+    this.state = {
+      date: new Date(),
+      title: "Title for App",
+      count: 0,
+    };
+    this.intervalFlag = setInterval(() => {
+      this.setState((state, props) => ({
+        date: new Date(),
+        count: state.count + 1,
+      }));
+    }, 1000);
+  }
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  componentDidMount() {
+    console.log("компонент был вызван");
+  }
+
+  componentDidUpdate() {
+    console.log("компонент был обновлен");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalFlag)
+    console.log('компонент был уничтожен');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
+        <span>{this.state.count}</span>
+        <p>{this.state.date.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       date: new Date(),
-//     };
-//   }
-//   render() {
-//     return <p>{this.state.date.toLocaleDateString()}</p>;
-//   }
-// }
-// ReactDOM.render(<Clock />, document.getElementById("root"));
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
