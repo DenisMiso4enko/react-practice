8193b0561d7cde2c7b1ef0cb805928ae5a574b66
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-11-16 23:01:19 +0300

    feat: add solution for events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import "./App.css";
+import React, { Component, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const handleClick = () => {
+    setCounter(counter + 1)
+  }
+  return <div>
+    <button onClick={handleClick}>На меня нажали {counter} раз</button>
+  </div>;
+};
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+//   handleClick() {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+//   render() {
+//     return <div>
+//       <button onClick={this.handleClick}>На меня нажали {this.state.count} раз</button>
+//     </div>;
+//   }
+// }
 
 export default App;
